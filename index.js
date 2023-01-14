3280bdbbe3c539ea4f646fc75dd6be7b02747b80
Skyhero-alt <skyhero.code@gmail.com>
2023-01-14 19:40:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const io = require("socket.io")(9999, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let users = [];
+
+const adduser = (userId, socketId) => {
+  !users.some((users) => users.userId == userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId != socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId == userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("adduser", (userId) => {
+    adduser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
